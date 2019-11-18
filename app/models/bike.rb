65cfdb96a9a0efce4f78f0d695d2193a4edad408
Silvia Patono <silvia.patono@gmail.com>
2019-11-18 16:15:07 +0100

    finished validation except photo: doesn't work

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,8 +1,14 @@
 class Bike < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+
   validates :name, presence: true
   validates :description, presence: true
-  validates :photo, presence: true
   validates :price_per_hour, presence: true, numericality: { only_integer: true }
+
+  validate :has_photo?
+
+  def has_photo?
+    photo.attached?
+  end
 end
