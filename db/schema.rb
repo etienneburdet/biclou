38d2b2634149b6b09e82077ad6144678b4287033
Bruno Valentin <bruno.valentin4@gmail.com>
2019-11-18 17:13:43 +0100

    added rental model and validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_142229) do
+ActiveRecord::Schema.define(version: 2019_11_18_155550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,17 @@ ActiveRecord::Schema.define(version: 2019_11_18_142229) do
     t.index ["user_id"], name: "index_bikes_on_user_id"
   end
 
+  create_table "rentals", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.bigint "user_id"
+    t.bigint "bike_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bike_id"], name: "index_rentals_on_bike_id"
+    t.index ["user_id"], name: "index_rentals_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -61,4 +72,6 @@ ActiveRecord::Schema.define(version: 2019_11_18_142229) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bikes", "users"
+  add_foreign_key "rentals", "bikes"
+  add_foreign_key "rentals", "users"
 end
