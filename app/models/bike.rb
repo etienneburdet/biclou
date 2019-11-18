6c0035071aeabaeb645e7d57b3d9d7f521ab585c
Silvia Patono <silvia.patono@gmail.com>
2019-11-18 15:39:13 +0100

    Creation bike model validations

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,4 +1,8 @@
 class Bike < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :photo, presence: true
+  validates :price_per_hour, presence: true, numericality: { only_integer: true }
 end
