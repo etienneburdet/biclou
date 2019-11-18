d370f86c07c3b34534186292152397da498d6fb6
Etienne <etienne.burdet@gmail.com>
2019-11-18 18:09:51 +0100

    added end date validation

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,6 +1,16 @@
 class Rental < ApplicationRecord
   belongs_to :user
   belongs_to :bike
+
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validates :end_after_start
+
+  private
+
+  def end_after_start
+    if start_date.present? && end_date.present? && (start_date < end_date)
+      errors.add(:end_date, "can't end before start")
+    end
+  end
 end
