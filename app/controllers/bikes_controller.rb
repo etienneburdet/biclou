e0447e16d51959ea533f4cf07c06b31f3cad2e35
Bruno Valentin <bruno.valentin4@gmail.com>
2019-11-19 11:03:48 +0100

    added bikes controller

---
+++ app/controllers/bikes_controller.rb
@@ -0,0 +1,36 @@
+class BikesController < ApplicationController
+  #def rent(rental_parameters = {})
+  #Rental.new(:start_date, :end_date, :bike_id)
+  def index
+    @bikes = Bike.all
+  end
+
+  def destroy
+    @bike = Bike.find(params[:bike])
+    @bike.destroy
+    redirect_to bikes_path
+  end
+
+  def update
+    @bike = Bike.find(bike_params)
+    @bike.update(params[:bike])
+    redirect_to bikes_path
+  end
+
+  def create
+    @bike = Bike.new(task_params)
+    @bike.save
+    redirect_to bikes_path
+  end
+
+  def new
+    @bike = Bike.new
+  end
+
+end
+
+  private
+
+    def bike_params
+      params.require(:bike).permit(:name, :description, :price_per_hour, :available)
+    end
