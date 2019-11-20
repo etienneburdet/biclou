67875d8f4b6ed395cecf98723d14d17ce968a6f5
Etienne <etienne.burdet@gmail.com>
2019-11-20 11:19:17 +0100

    fixed pundit for bikes & rentals : index, new, create (probably destroy)

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,18 +2,24 @@ class RentalsController < ApplicationController
   before_action :set_bike, only: :create
 
   def index
-    @rentals = Rental.all
+    @rentals = policy_scope(Rental).order(created_at: :desc)
   end
 
   def new
     @rental = Rental.new
+    authorize @rental
   end
 
   def create
     @rental = Rental.new(rental_params)
+    @rental.user = current_user
     @rental.bike = @bike
-    @rental.save
-    redirect_to rental_path(@rental)
+    authorize @rental
+    if @rental.save
+      redirect_to rental_path(@rental)
+    else
+      render :new
+    end
   end
 
   private
