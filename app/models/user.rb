e057564db394bcdda33b0b281d1f312e84398079
Etienne <etienne.burdet@gmail.com>
2019-11-21 07:21:40 +0100

    dependency in models hotfix. Seed works on heroku.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :bikes, dependent: :destroy
-  has_many :rentals, dependant: :destroy
+  has_many :rentals, dependent: :destroy
 end
